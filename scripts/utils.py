75f8361900f26b01f97e4919913b2944a1a0499e
shankal17 <63242166+shankal17@users.noreply.github.com>
2021-08-04 16:43:54 -0400

    Added some docstrings

--- scripts/utils.py
+++ scripts/utils.py
@@ -3,6 +3,30 @@ import torch.optim as optim
 import models as m
 
 def first_order_loss_with_ic(neural_network, a, g, ic, domain_lower_bound=0, domain_upper_bound=1, num_points=10):
+    """Computes loss for a given NN
+
+    Parameters
+    ----------
+    nerual_network : torch.nn.Module
+        Neural network used in trial solution
+    a: function
+        Function of coefficient in first-order linear DE
+    g : function
+        Function on right-hand side of DE
+    ic : float
+        Initial condition of DE
+    domain_lower_bout : float, optional
+        Lower limit of the DE domain considered
+    domain_upper_bound : float, optional
+        Upper limit of the DE domain considered
+    num_points : int, optional
+        Number of points in which to split the domain
+    
+    Returns
+    -------
+    torch.tensor
+        resulting loss of trial solution in it's current state
+    """
 
     def trial_solution(x):
         result = ic + x * neural_network(x)
@@ -18,6 +42,34 @@ def first_order_loss_with_ic(neural_network, a, g, ic, domain_lower_bound=0, dom
     return torch.sum(individual_error)
 
 def second_order_loss_with_ic(neural_network, a, b, g, ic, ic_prime, domain_lower_bound=0, domain_upper_bound=1, num_points=10):
+    """Computes loss for a given NN
+
+    Parameters
+    ----------
+    nerual_network : torch.nn.Module
+        Neural network used in trial solution
+    a: function
+        Function of coefficient in first-order linear DE
+    b: function
+        Function of coefficient in first-order linear DE
+    g : function
+        Function on right-hand side of DE
+    ic : float
+        Initial condition of DE solution
+    ic_prime : float
+        Initial condition on the derivative of DE solution
+    domain_lower_bout : float, optional
+        Lower limit of the DE domain considered
+    domain_upper_bound : float, optional
+        Upper limit of the DE domain considered
+    num_points : int, optional
+        Number of points in which to split the domain
+    
+    Returns
+    -------
+    torch.tensor
+        resulting loss of trial solution in it's current state
+    """
 
     def trial_solution(x):
         result = ic + x*ic_prime + (x**2) * neural_network(x)
@@ -29,4 +81,5 @@ def second_order_loss_with_ic(neural_network, a, b, g, ic, ic_prime, domain_lowe
     dtrial_dx = (trial_solution(x+delta) - trial_solution(x)) / delta
     d2trial_dx2 = (trial_solution(x+delta) - 2*trial_solution(x) + trial_solution(x-delta))/(delta**2)
     individual_error = (d2trial_dx2 - (g(x) - a(x)*dtrial_dx - b(x)*trial_solution(x)))**2
+    
     return torch.sum(individual_error)
